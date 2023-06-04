ccc4820f469530bf4ae398dc17be9bf316105cad
Valery <ilchenko.valery.developer@gmail.com>
2023-06-04 16:46:38 +0300

    fetched data from a server, deleted dummy products

--- client/src/pages/HamePage.tsx
+++ client/src/pages/HamePage.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Grid, Typography } from '@mui/material';
 
-import { products } from '../products';
+import { Product } from '../interfaces';
+
 import ProductCard from '../components/ProductCard';
 
 const HamePage = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
-      <Typography variant='h4'>Latest Product</Typography>
+      <Typography variant="h4">Latest Product</Typography>
       <Grid container spacing={3}>
         {products.map((product) => (
           <Grid key={product._id} item xs={12} sm={6} md={4} xl={3}>
