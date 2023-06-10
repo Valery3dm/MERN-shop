99684c670bac8105a643dc6b3e2ab3329f3fdc2c
Valery <ilchenko.valery.developer@gmail.com>
2023-06-10 22:15:40 +0300

    added errorMiddleware, errorHandler

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -13,9 +13,12 @@ router.get('/', asyncHandler(async (req, res) => {
 router.get('/:id', asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
-  if (product) return res.json(product);
-
-  res.status(404).json({ message: 'Product not found' });
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404)
+    throw new Error('Resource not fount')
+  }
 }));
 
 export default router;
