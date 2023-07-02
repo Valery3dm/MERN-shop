c8092fc6b2cd822da6983831c1fd57b285887ccb
Valery <ilchenko.valery.developer@gmail.com>
2023-07-02 20:02:44 +0300

    added User Routes, Controllers

--- server/server.js
+++ server/server.js
@@ -3,19 +3,26 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 connectDB() // Connect to DB
+
 const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 })
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
