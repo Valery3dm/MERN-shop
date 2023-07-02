36752d701e2964ff11d2543cd89ecd4cbcb6c13b
Valery <ilchenko.valery.developer@gmail.com>
2023-07-02 22:14:32 +0300

    added Encryption for User Schema. Register and logout controllers

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcryptjs from 'bcryptjs';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,8 +25,17 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcryptjs.compare(enteredPassword, this.password);
+  return await bcrypt.compare(enteredPassword, this.password);
 }
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 
