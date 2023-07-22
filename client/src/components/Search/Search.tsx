bb0fa21d6ab1af480455ee15fe0b202e4ce708f6
Valery <ilchenko.valery.developer@gmail.com>
2023-07-22 18:39:33 +0300

    added search component

---
+++ client/src/components/Search/Search.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { useNavigate, useParams } from 'react-router';
+import {
+  Box,
+  FormControl,
+  Button,
+  Input,
+  InputLabel
+} from '@mui/material';
+
+import styles from './Search.module.scss';
+
+const Search = () => {
+  const navigate = useNavigate();
+  const {keyword: urlKeyWord} = useParams();
+  const [keyword, setKeyWord] = useState(urlKeyWord || '');
+
+  const onSubmit = () => {
+    if (keyword.trim()) {
+      navigate(`/search/${keyword}`);
+      setKeyWord('');
+    } else {
+      navigate(`/`);
+    }
+  }
+  
+  return (
+    <Box component="form" noValidate autoComplete="off" className={styles.searchWrapper}>
+      <FormControl margin="normal" fullWidth>
+        <Input
+          id="search-input"
+          type="text"
+          placeholder='Search Products...'
+          aria-describedby="search-helper-text"
+          className={styles.searchInput}
+          value={keyword}
+          onChange={(e) => setKeyWord(e.target.value)}
+        />
+      </FormControl>
+      <Button
+        variant="contained"
+        className={styles.searchBtn}
+        onClick={onSubmit}
+      >
+        Search
+      </Button>
+    </Box>
+  );
+};
+
+export default Search;
