fbe91cd1b24202390cb76fd8155e6e67abc7c82e
Valery <ilchenko.valery.developer@gmail.com>
2023-07-29 14:54:20 +0300

    fix: displaying title on the order page

--- client/src/pages/OrderPage/OrderPage.tsx
+++ client/src/pages/OrderPage/OrderPage.tsx
@@ -141,7 +141,16 @@ const OrderPage = () => {
 
   return (
     <>
-      <Typography variant="h4">Order {order?._id}</Typography>
+      <Typography
+        variant="h4"
+        sx={{
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        }}
+      >
+        Order {order?._id}
+      </Typography>
 
       <Grid container spacing={2}>
         <Grid item xs={12} sm={12} md={8} xl={9}>
